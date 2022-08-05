95129e624533fa671749a492c6a42600f71c5728
Kanon Tromp <ktromp1.student@gmail.com>
2022-08-05 12:51:08 -0700

    Uploading initial files for website

---
+++ Quotes_Bao_S2S.js
@@ -0,0 +1,45 @@
+function round50(x, base = 50){
+    return base * Math.round(x / base);
+}
+
+function runQuotes(){
+    const s2sMax = 900;
+    const s2sMin = 400;
+    const baoMax = 1800;
+    const baoMin = 700;
+    const milesMax = 3300;
+    const milesMin = 300;
+    const s2sVariant = s2sMax - s2sMin;
+    const baoVariant = baoMax - baoMin;
+    const milesVariant = milesMax - milesMin;
+    // tripMileage = int(input("\n\nMiles?\n\n\n")) in python
+    let tripMileage = prompt("Please enter the total mileage of the trip");
+    const sTimeFrames = ["2 to 3 weeks",
+    "2.5 to 3.5 weeks", "3 to 4 weeks"];
+    const bTimeFrames = ["1 to 2 weeks", "1.5 to 2.5 weeks"];
+    const sTripQuote = round50(
+        s2sMax - (((milesVariant - tripMileage) / milesVariant) * s2sVariant));
+    const bTripQuote = round50(
+        baoMax - (((milesVariant - tripMileage) / milesVariant) * baoVariant));
+    document.getElementById("quotes").innerHTML = `<br>Bao quote to Iconic (without markup): ${bTripQuote}<br>Bao timeframe: Roughly ${bTimeFrames[bTiming(tripMileage)]}<br>S2S quote to Iconic (without markup): ${sTripQuote}<br>S2S timeframe: Roughly ${sTimeFrames[sTiming(tripMileage)]}<br><br><br>`;
+    document.getElementById("clientCopyPaste").innerHTML = `<br>Here is the format to paste to the client:<br><br>$${bTripQuote}(NEEDS MARKUP) - Roughly ${bTimeFrames[bTiming(tripMileage)]} - Private Shipper<br>$${sTripQuote}(NEEDS MARKUP) - Roughly ${sTimeFrames[sTiming(tripMileage)]} - Private Shipper<br>$TBD - Roughly 5 - 6 weeks - Haulbikes`;
+    /** 
+     * doc write the following:
+     * print("\n\n\nBao quote to Iconic (without markup): {}\nBao timeframe: Roughly {}\nS2S quote to Iconic (without markup): {}\nS2S timeframe: Roughly {}\n\n\n".format(
+        bTripQuote, bTimeFrames[bTiming(tripMileage)], sTripQuote, sTimeFrames[sTiming(tripMileage)]))
+    print("\nHere is the format to paste to the client:\n\n${}(NEEDS MARKUP) - Roughly {} - Private Shipper\n${}(NEEDS MARKUP) - Roughly {} - Private Shipper\n$TBD - Roughly 5 - 6 weeks - Haulbikes\n\n\n".format(
+        bTripQuote, bTimeFrames[bTiming(tripMileage)], sTripQuote, sTimeFrames[sTiming(tripMileage)]))
+    */
+    
+
+}
+
+function sTiming(miles){
+    if (miles >= 300 && miles < 750) return 0;
+    else if (miles >= 751 && miles <= 1500) return 1;
+    else return 2;
+}
+
+function bTiming(miles){
+    return miles >= 2000 ? 1 : 0;
+}
